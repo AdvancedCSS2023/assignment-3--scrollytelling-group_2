1fa6c111c1b0a5263a687063816fd1dfda3b2561
Chris <ngo.chris@hotmail.com>
2023-05-02 20:37:16 +0200

    intersection observer, stickying and unstickying element

---
+++ script.js
@@ -0,0 +1,40 @@
+const sections = document.querySelectorAll('.unSticky__sensor');
+
+let laptopScreenIO = document.querySelector('.laptop__screen--IO');
+const options = {};
+
+const elementToObserve = document.querySelector('.laptop__screen');
+
+
+const getSticky = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+        console.log('Element is fully visible');
+        laptopScreenIO.style.position = 'sticky';
+        elementToObserve.style.overflow ='auto'
+      }
+      else{
+        elementToObserve.style.overflow ='hidden'
+        laptopScreenIO.style.position = 'static';
+      }
+    });
+  }, {threshold: [1]});
+
+getSticky.observe(elementToObserve);
+
+const headline = document.querySelectorAll('.headline--top');
+
+const unSticky = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting){
+            return;
+        }
+
+        laptopScreenIO.style.position = 'static';
+    });
+}, options);
+
+sections.forEach((section) => {
+    unSticky.observe(section);
+});
